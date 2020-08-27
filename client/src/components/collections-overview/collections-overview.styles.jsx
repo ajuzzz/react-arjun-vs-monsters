86d3758811c52a5e64b40387201c16ea6695a2a0
arjun.p <arjun.p@thinkplam.com>
2020-08-27 16:57:11 +0530

    backend server aded

---
+++ client/src/components/collections-overview/collections-overview.styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const CollectionsOverviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (max-width: 800px) {
+    align-items: center;
+    display: grid;
+    // grid-template-columns: 1fr 1fr;
+    grid-gap: 15px;
+
+  }
+
+`;
---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HIsvDBRd3Ceexj54yKLro5yTLZXA8J43OC0hjHhxlGMmJ987am2V6aZHAXY6oFhXu2u8eOycf1gDp0nLnlHjgf800rGJHLgha';
+
+  const onToken = token => {
+    axios({
+      url: 'checkout',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', token);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='React Training.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
